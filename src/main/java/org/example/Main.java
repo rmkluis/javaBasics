adf7a37bce22f156c4039032b5f69406a7446789
Robin Kluis <faultic@gmail.com>
2023-01-05 11:22:16 -0600

    exercise complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String fullName = args[0];
-        String email = args[1];
-        System.out.println(fullName + " " + email);
+        if (args.length < 1){
+            System.out.println("Please specify a name and email");
+        } else if (args.length < 2) {
+            System.out.printf("Please specify an email for %s", args[0]).println();
+        } else {
+            System.out.printf("%s <%s>", args[0], args[1]).println();
+        }
     }
 }
