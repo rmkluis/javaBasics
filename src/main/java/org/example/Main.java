fac483431551233b719d2a604cf525c10fd7c2f8
Robin Kluis <faultic@gmail.com>
2023-01-05 11:05:57 -0600

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String fullName = args[0];
+        String email = args[1];
+        System.out.println(fullName + " " + email);
+    }
+}
